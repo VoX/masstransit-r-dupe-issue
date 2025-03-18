41a383a08a85965eabe638119f1616a726cf2ac2
Ian Hills <ihills@draftkings.com>
2025-03-18 11:58:07 -0400

    update with new r-dupe issue

--- Program.cs
+++ Program.cs
@@ -2,14 +2,16 @@ namespace AmazonSqsReceiveEndpointExample;
 
 using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 public class Program
 {
     public static async Task Main()
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder();
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 
         var services = builder.Services;
         services.AddMassTransit(x =>
@@ -36,8 +38,11 @@ public class Program
         var provider = host.Services;
         var bus = provider.GetService<IBus>() as IBusControl;
 
-        await bus.StartAsync();
-        await bus.Publish(new TestMessage { Id = "1" });
+        var queue = new Uri("queue:masstransit-test-q");
+        var sendEndpoint = await bus.GetSendEndpoint(queue);
+
+        await sendEndpoint.Send(new TestMessage { Id = "message 1" });
+        await sendEndpoint.Send(new TestMessage { Id = "message 2" });
 
         Console.ReadLine();
         await host.StopAsync();
@@ -46,18 +51,18 @@ public class Program
 
 public class TestConsumer : IConsumer<TestMessage>
 {
-    private readonly ILogger<TestConsumer> logger;
+    private static int _count = 0;
 
-    public TestConsumer(ILogger<TestConsumer> logger)
-    {
-        this.logger = logger;
-    }
     public async Task Consume(ConsumeContext<TestMessage> context)
     {
-        await Task.Delay(TimeSpan.FromSeconds(1));
+        if (Interlocked.Increment(ref _count) == 2)
+        {
+            // wait more than 30 seconds at this breakpoint before continuing execution
+            // if done correctly you should see a R-DUPE warning in the logs
+            Debugger.Break();
+        }
 
-        var redelivered = context.ReceiveContext.Redelivered ? "redelivered" : "";
-        logger.LogInformation($"Got Message {context.Message.Id} {redelivered}");
+        await Task.Delay(TimeSpan.FromSeconds(5));
 
         throw new Exception();
     }
